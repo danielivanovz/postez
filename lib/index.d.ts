1a561a5344eef5720b0a53bb76af4e5ebcb70658
Daniel Ivanov <daniel.ivanov@mavigex.com>
2022-04-26 11:59:42 +0200

    - cleanup;

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,4 @@
 import pg from './db';
 import * as Types from './types';
 import { main } from './utilities';
-export declare namespace Postez {
-    const postgrez: typeof main;
-    const pgp: typeof pg;
-    type IDatabaseConfiguration = Types.IDatabaseConfiguration;
-    type ISchema = Types.TSchema;
-}
+export default main
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
-import * as dotenv from 'dotenv';
-
 import pg from './db';
 import * as Types from './types';
 import { main } from './utilities';
 
-dotenv.config();
+// export declare namespace Postez {
+//   export const postgrez: typeof main;
+//   export const pgp: typeof pg;
+//   export const db: typeof pg['db'];
+//   export type IDatabaseConfiguration = Types.IDatabaseConfiguration;
+//   export type ISchema = Types.TSchema;
+// }
 
-const config: Postez.IDatabaseConfiguration = {
-  host: 'localhost',
-  port: process.env.PORT,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-};
-
-export declare namespace Postez {
-  export const postgrez: typeof main;
-  export const pgp: typeof pg;
-  export type IDatabaseConfiguration = Types.IDatabaseConfiguration;
-  export type ISchema = Types.TSchema;
+export function postez(db, path) {
+  return main(db, path);
 }
